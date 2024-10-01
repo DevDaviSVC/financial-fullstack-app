eb2ed0a3fd1bc2e1f2e76abf818830b0b337a46f
Davi Souza <davisouzavieiradacosta@gmail.com>
2024-10-01 20:31:54 -0300

    API bug fix

--- backend/server.js
+++ backend/server.js
@@ -21,16 +21,15 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/dashboard", dashboardRoutes);
+
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${process.env.PORT}!`);
